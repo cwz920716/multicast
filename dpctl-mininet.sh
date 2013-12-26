7988a9864f805c15fb20234e60b26cf4c4c225af
Wenzhi Cui <cwz920716@gmail.com>
2013-12-26 01:21:05 -0800

    Mininet

--- dpctl-mininet.sh
+++ dpctl-mininet.sh
@@ -1,3 +1,7 @@
-dpctl add-flow tcp:127.0.0.1:6634 in_port=1,actions=output:3
-dpctl add-flow tcp:127.0.0.1:6634 in_port=2,actions=output:3
-dpctl add-flow tcp:127.0.0.1:6634 in_port=3,actions=output:1,2
+#! /bin/sh
+
+sudo dpctl del-flows tcp:127.0.0.1:6634 
+
+sudo dpctl add-flow tcp:127.0.0.1:6634 in_port=1,idle_timeout=0,actions=output:3
+sudo dpctl add-flow tcp:127.0.0.1:6634 in_port=2,idle_timeout=0,actions=output:2,output:1,output:3
+sudo dpctl add-flow tcp:127.0.0.1:6634 in_port=3,idle_timeout=0,actions=output:1,output:2
